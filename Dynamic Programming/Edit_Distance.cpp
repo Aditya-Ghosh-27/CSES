f401e8f438f66badfd87221eecf515e382e4f44a
Aditya Ghosh <adi27.ghosh@gmail.com>
2026-04-19 01:27:03 +0530

    solved edit distance q from cses

--- Dynamic Programming/Edit_Distance.cpp
+++ Dynamic Programming/Edit_Distance.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 #define int long long
 
-void solve(){
+// dp[i][j]: edit distance b/w first i chars of s and first j chars of t
 
+void solve(){
+    string s, t;
+    cin >> s >> t;
+    int n = s.length();
+    int m = t.length();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));
+
+    for(int i = 0; i <= n; i++) dp[i][0] = i;
+    for(int j = 0; j <= m; j++) dp[0][j] = j;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(s[i - 1] == t[j - 1]){
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+            }
+        }
+    }
+
+    cout << dp[n][m] << '\n';
 }
 
 signed main(){
